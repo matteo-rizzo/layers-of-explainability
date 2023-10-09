fdd2eb3e99bbd963897666200393aeaafaeca6c2
Matteo Marcuzzo <matteo.marcuzzo.it@gmail.com>
2023-10-09 17:56:05 +0200

    Small fixes

--- src/layered_explainability_strategy/pipeline.py
+++ src/layered_explainability_strategy/pipeline.py
@@ -1,5 +1,7 @@
 from scipy.special import softmax
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, logging
+
+logging.set_verbosity_error()
 
 
 # Preprocess text (username and link placeholders)
@@ -24,15 +26,15 @@ def run_model(model, task, text, labels):
     scores = output[0][0].detach().numpy()
     scores = softmax(scores)
 
-    print(f"*** {task} ***")
+    print(f"*** {task.title()} ***")
     for s, l in zip(scores, labels):
         print(f"{l.title():<10}: {s:.3f}")
 
 
 def main():
-    text = "What takes up 12 parking spaces? 6 women drivers"
+    text = "Pro Tip- Put all your bitches on a period tracker app so you know when to avoid they ass"
     print("Text:")
-    print(text)
+    print(f"'{text}'")
     # -----------------------------------------------
     task = 'irony'  # irony, non irony
     model = 'cardiffnlp/twitter-roberta-base-irony'
