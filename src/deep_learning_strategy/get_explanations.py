d2d0ae61c502abc773a0d266e6ac7e32aac57f63
alek9z <alessandro.zangari.code@outlook.com>
2023-10-09 15:53:54 +0200

    TranShap pipeline

--- src/deep_learning_strategy/get_explanations.py
+++ src/deep_learning_strategy/get_explanations.py
@@ -1,10 +1,10 @@
 from src.utils.ami_2020_scripts.dataset_handling import train_val_test
 from src.deep_learning_strategy.classes.HuggingFacePipeline import HuggingFacePipeline
 
-from src.deep_learning_strategy.utils import shap_explain
+from src.deep_learning_strategy.explainations_utils import shap_explain, transhap_explain
 from src.utils.yaml_manager import load_yaml
 
-NUM_EXPLANATIONS = 10
+NUM_EXPLANATIONS = 100
 
 if __name__ == "__main__":
     config: dict = load_yaml("src/deep_learning_strategy/config.yml")
@@ -16,4 +16,6 @@ if __name__ == "__main__":
     dataset_m = train_val_test(target="M")
 
     hf_pipeline = pipe_m.get()
-    shap_explain(dataset_m["test"]["x"][:NUM_EXPLANATIONS], model=hf_pipeline, tokenizer=hf_pipeline.tokenizer, target_label=target_label)
+    # shap_explain(dataset_m["test"]["x"][:NUM_EXPLANATIONS], model=hf_pipeline, tokenizer=hf_pipeline.tokenizer, target_label=target_label)
+    transhap_explain(dataset_m["test"]["x"][:NUM_EXPLANATIONS], model=hf_pipeline, tokenizer=hf_pipeline.tokenizer, target_label=target_label,
+                     device="cuda" if config["use_gpu"] else "cpu")
--- src/deep_learning_strategy/utils.py
+++ src/deep_learning_strategy/utils.py
@@ -8,7 +8,6 @@ from typing import Callable
 
 import evaluate
 import numpy as np
-import shap
 from datasets import Dataset, NamedSplit, Features, Value, ClassLabel
 from matplotlib import pyplot as plt
 from transformers import Trainer
@@ -113,33 +112,3 @@ def delete_checkpoints(path: Path | str) -> None:
         if item_path.is_dir() and re.match(r"^checkpoint-\d+$", item):
             # Delete the directory
             shutil.rmtree(item_path)
-
-
-def shap_explain(texts: list[str], model, tokenizer, target_label: str, show: bool = True):
-    pmodel = shap.models.TransformersPipeline(model, rescale_to_logits=True)
-
-    explainer = shap.Explainer(pmodel, tokenizer)
-    shap_values = explainer(texts)
-
-    target_dir = Path("plots") / "nlp"
-    target_dir.mkdir(parents=True, exist_ok=True)
-
-    # fig = plt.figure()
-    # shap.plots.text(shap_values[0, :, target_label])
-
-    for i, t in enumerate(texts):
-        shap.plots.bar(shap_values[i, :, target_label], max_display=25, show=False)
-        plt.title(f"Sentence: {t}")
-        plt.gcf().set_size_inches(10, 14)
-        plt.tight_layout()
-        plt.savefig(target_dir / f"shap_sentence_barplot_{i}.png", dpi=400)
-        plt.clf()
-
-    shap.plots.bar(shap_values[:, :, target_label].mean(0), max_display=25, show=False)
-    plt.title("Mean relative importance of tokens for classification")
-    plt.gcf().set_size_inches(10, 14)
-    plt.tight_layout()
-
-    plt.savefig(target_dir / "shap_mean_barplot.png", dpi=400)
-    if show:
-        plt.show()
