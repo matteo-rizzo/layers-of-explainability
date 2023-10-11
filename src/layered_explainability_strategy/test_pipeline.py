61b73cf1bfdd462ff0449df80871853e7592783a
Matteo Marcuzzo <matteo.marcuzzo.it@gmail.com>
2023-10-11 11:39:32 +0200

    Updated test pipeline with sarcasm

--- src/layered_explainability_strategy/test_pipeline.py
+++ src/layered_explainability_strategy/test_pipeline.py
@@ -1,5 +1,5 @@
 from scipy.special import softmax
-from transformers import AutoTokenizer, AutoModelForSequenceClassification, logging
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, AutoModelForSeq2SeqLM, logging
 
 logging.set_verbosity_error()
 
@@ -31,6 +31,25 @@ def run_model(model, task, text, labels):
         print(f"{l.title():<10}: {s:.3f}")
 
 
+def run_t5_model(model, task, text):
+    print("-------------------------------------------------------")
+    tokenizer = AutoTokenizer.from_pretrained(model)
+    model = AutoModelForSeq2SeqLM.from_pretrained(model)
+    input_ids = tokenizer.encode(text + '</s>', return_tensors='pt')
+    output = model.generate(input_ids=input_ids, max_length=3)
+
+    dec = [tokenizer.decode(ids) for ids in output]
+
+    if "derision" in dec[0]:
+        label = "sarcasm"
+    elif "normal" in dec[0]:
+        label = "non-sarcasm"
+    else:
+        raise ValueError("Error in T5 output")
+    print(f"*** {task.title()} ***")
+    print(f"{label.title():<10}")
+
+
 def main():
     text = "Pro Tip- Put all your bitches on a period tracker app so you know when to avoid they ass"
     print("Text:")
@@ -41,17 +60,26 @@ def main():
     labels = ['non-irony', 'irony']
     run_model(model, task, text, labels)
     # -----------------------------------------------
-    task = 'sentiment-latest'  # negative, neutral, positive
-    model = 'cardiffnlp/twitter-roberta-base-sentiment-latest'
-    labels = ['negative', 'neutral', 'positive']
+    # task = 'sarcasm'  # sarcasm, non sarcasm
+    # model = 'mrm8488/t5-base-finetuned-sarcasm-twitter'
+    # run_t5_model(model, task, text)
+    # # -----------------------------------------------
+    task = 'sarcasm'  # sarcasm, non sarcasm
+    model = 'helinivan/english-sarcasm-detector'
+    labels = ['non-sarcasm', 'sarcasm']
     run_model(model, task, text, labels)
-    # task = 'polarity'  # 0 negative, 1 positive
-    # model = 'VictorSanh/roberta-base-finetuned-yelp-polarity'
     # -----------------------------------------------
-    task = 'hate'  # non-hate / hate
-    model = 'cardiffnlp/twitter-roberta-base-hate'
-    labels = ['non-hate', 'hate']
-    run_model(model, task, text, labels)
+    # task = 'sentiment-latest'  # negative, neutral, positive
+    # model = 'cardiffnlp/twitter-roberta-base-sentiment-latest'
+    # labels = ['negative', 'neutral', 'positive']
+    # run_model(model, task, text, labels)
+    # # task = 'polarity'  # 0 negative, 1 positive
+    # # model = 'VictorSanh/roberta-base-finetuned-yelp-polarity'
+    # # -----------------------------------------------
+    # task = 'hate'  # non-hate / hate
+    # model = 'cardiffnlp/twitter-roberta-base-hate'
+    # labels = ['non-hate', 'hate']
+    # run_model(model, task, text, labels)
     # -----------------------------------------------
     task = 'offensive'  # non-offensive, offensive
     model = 'cardiffnlp/twitter-roberta-base-offensive'
