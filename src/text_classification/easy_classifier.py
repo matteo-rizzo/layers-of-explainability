5b474a093d42df5bc80f15967d1012fd0eb8ad73
alek9z <alessandro.zangari.code@outlook.com>
2023-10-27 10:12:58 +0200

    Training, testing, grid search of standard/ensemble clf on arbitrary dataset

--- src/text_classification/easy_classifier.py
+++ src/text_classification/easy_classifier.py
@@ -1,55 +1,43 @@
 """ Script to GS and fit a classifier on review dataset, to use as feature extractor """
 from __future__ import annotations
 
+import copy
 import pickle
 from pathlib import Path
 from pprint import pprint
-from typing import Type
 
 import numpy as np
 import pandas as pd
 import torch
 from sklearn.base import ClassifierMixin
-from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, HistGradientBoostingClassifier
+from sklearn.linear_model import RidgeClassifier, LogisticRegression
 from sklearn.model_selection import GridSearchCV, train_test_split
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import LabelEncoder
 from sklearn.tree import DecisionTreeClassifier
 from torch import nn
 
-from src.deep_learning_strategy.classes.AMI2018Dataset import AMI2018Dataset
 from src.deep_learning_strategy.classes.CGReviewDataset import CGReviewDataset
 from src.deep_learning_strategy.classes.Dataset import AbcDataset
-from src.explainable_strategy.pipeline import make_pipeline
 from src.text_classification.classes.MLP import MLP
 from src.text_classification.main import compute_metrics
 from src.utils.yaml_manager import load_yaml
 
-sk_classifier_type = RandomForestClassifier
-dataset: AbcDataset = CGReviewDataset()  # AMI2018Dataset()
+SK_CLASSIFIER_TYPE: type = AdaBoostClassifier
+SK_CLASSIFIER_PARAMS: dict = dict(estimator=LogisticRegression())
+# RandomForestClassifier()
+DATASET: AbcDataset = CGReviewDataset()  # AMI2018Dataset()
+DO_GRID_SEARCH = True
 
 
-def naive_classifier(sk_classifier: ClassifierMixin, training_data: pd.DataFrame, params=dict()) -> np.ndarray | tuple[np.ndarray, Pipeline]:
-    pipe = make_pipeline(sk_classifier)
+def grid_search_best_params(training_data: pd.DataFrame, train_config):
+    """
+    Perform grid search of the selected classifier (see 'SK_CLASSIFIER_TYPE').
 
-    print("------ Training")
-
-    pipe.fit(training_data["text_"], training_data["label"])
-
-    predicted = None
-    # if predict:
-    #     print("------ Testing")
-    #
-    #     # Predicting with a test dataset
-    #     predicted = pipe.predict(training_data["test"]["x"])
-    #
-    # if not return_pipe:
-    #     return predicted
-    # else:
-    return predicted, pipe
-
-
-def grid_search_best_params(sk_classifier_type: Type[ClassifierMixin], data: pd.DataFrame, train_config):
+    @param training_data: dataframe with training data. Target should be in 'y' column;
+    @param train_config: training configuration with parameters for GS and for classifier.
+    """
     # Load configuration
     num_rand_states: int = train_config["grid_search_params"]["num_seeds"]
     test_size: float = train_config["grid_search_params"]["test_size"]
@@ -61,19 +49,24 @@ def grid_search_best_params(sk_classifier_type: Type[ClassifierMixin], data: pd.
     # Per seed training
     for rs in range(num_rand_states):
         # Prepare splits
-        train_df, val_df = train_test_split(data, random_state=0, shuffle=True, stratify=data["label"], test_size=test_size)
+        train_df, val_df = train_test_split(training_data, random_state=0, shuffle=True, stratify=training_data["y"].tolist(), test_size=test_size)
+
+        y_train = train_df.pop("y")
+        y_val = val_df.pop("y")
 
         # Setup and train classifier
-        params = train_config["grid_search_params"][sk_classifier_type.__name__]
+        params = train_config["grid_search_params"][SK_CLASSIFIER_TYPE.__name__]
+        base_estimator_params = train_config[SK_CLASSIFIER_TYPE.__name__]
 
-        gs = GridSearchCV(sk_classifier_type(), param_grid=params, verbose=10, refit=True, n_jobs=-1, cv=5)
-        grid_clf = make_pipeline(gs)
+        gs = GridSearchCV(SK_CLASSIFIER_TYPE(**SK_CLASSIFIER_PARAMS), param_grid=params, verbose=10, refit=True, n_jobs=-1, cv=5)
+        # Set base parameters in the estimator inside GS
+        gs.estimator.set_params(**base_estimator_params)
 
-        grid_clf.fit(train_df["text_"], train_df["label"])
-        y_pred = grid_clf.predict(val_df["text_"]).tolist()
+        gs.fit(train_df, y_train)
+        y_pred = gs.predict(val_df).tolist()
 
         # Calculate metrics
-        metrics = compute_metrics(y_pred, val_df["label"])
+        metrics = compute_metrics(y_pred, y_val)
 
         # Print results
         print(f"Random Seed {rs} - Validation Metrics:")
@@ -85,7 +78,12 @@ def grid_search_best_params(sk_classifier_type: Type[ClassifierMixin], data: pd.
         avg_metrics["recall"].append(metrics["recall"])
         avg_metrics["f1"].append(metrics["f1"])
 
-        best_params.append(gs.best_params_)
+        params = {
+            "gs_params": copy.deepcopy(gs.best_params_),
+            "all_params": gs.get_params(True)
+        }
+
+        best_params.append(params)
 
     print("-----------------------------------------------------------")
     print(f"Average Validation Metrics Over {num_rand_states} Random Seeds:")
@@ -95,6 +93,24 @@ def grid_search_best_params(sk_classifier_type: Type[ClassifierMixin], data: pd.
     print("-----------------------------------------------------------")
     pprint(best_params)
 
+    return best_params
+
+
+def update_params_composite_classifiers(train_config: dict) -> dict:
+    """
+    Some classifiers (ensemble, boosting, etc.) may need specific configuration depending on the type.
+    For instance, AdaBoost takes an "estimator" argument to set the base estimator.
+    This cannot be specified in YAML, and each estimator can have its hyperparameters and grid search params.
+    Hence, this method updates the configuration of AdaBoost with all parameters of nested classifiers.
+    """
+    if SK_CLASSIFIER_TYPE in [AdaBoostClassifier]:  # potentially works for other "nested" cases
+        base_est_name = SK_CLASSIFIER_PARAMS.setdefault("estimator", DecisionTreeClassifier()).__class__.__name__
+        base_est_config = {f"estimator__{k}": v for k, v in train_config[base_est_name].items()}
+        base_est_gs_config = {f"estimator__{k}": vs for k, vs in train_config["grid_search_params"][base_est_name].items()}
+        train_config[f"{SK_CLASSIFIER_TYPE.__name__}"].update(base_est_config)
+        train_config["grid_search_params"][f"{SK_CLASSIFIER_TYPE.__name__}"].update(base_est_gs_config)
+    return train_config
+
 
 def target_conversion(x: torch.Tensor):
     return np.round(torch.sigmoid(x).detach().numpy())
@@ -102,7 +118,7 @@ def target_conversion(x: torch.Tensor):
 
 def neural_classifier(train_data: pd.DataFrame, test_data: pd.DataFrame) -> None:
     from skorch import NeuralNetBinaryClassifier
-    from skorch.callbacks import EarlyStopping, Checkpoint, EpochScoring
+    from skorch.callbacks import EarlyStopping, Checkpoint
 
     layers = [
         (512, 0.3, True, nn.ReLU()),
@@ -162,80 +178,78 @@ def neural_classifier(train_data: pd.DataFrame, test_data: pd.DataFrame) -> None
     compute_metrics(y_pred, y_test.tolist(), sk_classifier_name="MLP Classifier")
 
 
-def final_ensemble(feature_classifier: ClassifierMixin,
-                   train_data: pd.DataFrame, test_data: pd.DataFrame,
-                   tfidf_clf: RandomForestClassifier = None) -> None:
-    # Add clf predictions
-    # probs = tfidf_clf.predict_proba(all_data["text_"].tolist())
-    #
-    # feature_clf = {
-    #     "score_0": probs[:, 0].tolist(),
-    #     "score_1": probs[:, 1].tolist()
-    # }
-    #
-    # f_df.update(feature_clf)
+def load_encode_dataset() -> tuple[pd.DataFrame, pd.DataFrame]:
+    """
+    Load dataset composed of extracted features based on the 'DATASET' global params that must be set to an AbcDataset object.
 
-    # train_df, test_df, train_y, test_y = train_test_split(f_df, all_data["label"], random_state=31, shuffle=True, stratify=all_data["label"], test_size=0.25)
-
-    y_train = train_data.pop("y")
-    y_test = test_data.pop("y")
-
-    feature_classifier.fit(train_data, y=y_train.tolist())
-
-    y_pred = feature_classifier.predict(test_data).tolist()
-
-    print("Metrics AFTER ensemble")
-    compute_metrics(y_pred, y_test.tolist(), sk_classifier_name="Final ENSEMBLE")
-
-
-def adaboost_classifier(train_config, training_data: pd.DataFrame, testing_data: pd.DataFrame):
-    base_clf = DecisionTreeClassifier(**train_config["DecisionTreeClassifier"])
-    clf = AdaBoostClassifier(n_estimators=5000, random_state=11, estimator=base_clf)
-
-    y_train = training_data.pop("y")
-    y_test = testing_data.pop("y")
-
-    clf.fit(training_data, y=y_train.tolist())
-
-    y_pred = clf.predict(testing_data).tolist()
-
-    print("Metrics AFTER AdaBoost")
-    compute_metrics(y_pred, y_test.tolist(), sk_classifier_name="Final ENSEMBLE")
-
-
-def main():
-    train_config: dict = load_yaml("src/text_classification/config/classifier.yml")
-    data_path_train = Path(dataset.BASE_DATASET) / f"{dataset.__class__.__name__}_train_features.csv"
-    data_path_test = Path(dataset.BASE_DATASET) / f"{dataset.__class__.__name__}_test_features.csv"
+    @return: two dataframes with training and test data; target label is set to class 'y' and is encoded with numbers in 0...n-1
+    """
+    data_path_train = Path(DATASET.BASE_DATASET) / f"{DATASET.__class__.__name__}_train_features.csv"
+    data_path_test = Path(DATASET.BASE_DATASET) / f"{DATASET.__class__.__name__}_test_features.csv"
 
     data_train = pd.read_csv(data_path_train)
     data_test = pd.read_csv(data_path_test)
 
-    data_train["y"] = dataset.get_train_labels()
-    data_test["y"] = dataset.get_test_labels()
+    if DATASET.__class__ == CGReviewDataset:
+        print("Removed CHATGpt detector features from the CGReviewDataset")
+        data_train.pop("ChatGPTDetector_Human")
+        data_train.pop("ChatGPTDetector_ChatGPT")
+        data_test.pop("ChatGPTDetector_Human")
+        data_test.pop("ChatGPTDetector_ChatGPT")
 
-    clf = sk_classifier_type(**train_config[sk_classifier_type.__name__])
+    train_labels = DATASET.get_train_labels()
+    test_labels = DATASET.get_test_labels()
 
-    # train_df, test_df = train_test_split(data, random_state=31, shuffle=True, stratify=data["label"], test_size=0.25)
-    #
-    # # grid_search_best_params(RandomForestClassifier, train_df, train_config)
+    # Binary encoding of labels
+    encoder = LabelEncoder()
+    y_train = encoder.fit_transform(train_labels)
+    y_test = encoder.transform(test_labels)
 
-    # Setup and train classifier
-    # _, clf = naive_classifier(RandomForestClassifier(**train_config["RandomForestClassifier"], n_jobs=-1), train_df)
-    #
-    # y_pred = clf.predict(test_df["text_"]).tolist()
-    #
-    # joblib.dump(clf, "dumps/text_classification/clf_pipeline.pkl", compress=1)
-    #
-    # # Calculate metrics
-    # print("Metrics before ensemble")
-    # compute_metrics(y_pred, test_df["label"], sk_classifier_name="Final RF")
+    data_train["y"] = y_train
+    data_test["y"] = y_test
+    return data_train, data_test
 
-    # adaboost_classifier(train_config, data_train, data_test)
 
-    # final_ensemble(clf, data_train, data_test)
+def train_classifier(data_train: pd.DataFrame, train_config: dict) -> ClassifierMixin:
+    """
+    Train a scikit-learn classifier on training data and return the fitted object.
+    """
+    clf = SK_CLASSIFIER_TYPE(**SK_CLASSIFIER_PARAMS, **train_config[SK_CLASSIFIER_TYPE.__name__])
 
-    neural_classifier(data_train, data_test)
+    y_train = data_train.pop("y")
+
+    clf.fit(data_train, y=y_train.tolist())
+
+    return clf
+
+
+def evaluate_metrics(clf: ClassifierMixin, data_test: pd.DataFrame):
+    """
+    Evaluate dataset metrics of a scikit-learn classifier on testing data.
+    """
+    y_test = data_test.pop("y")
+
+    y_pred = clf.predict(data_test).tolist()
+
+    print("Classification metrics")
+    DATASET.compute_metrics(y_pred, y_test, sk_classifier_name=clf.__class__.__name__)
+
+
+def main():
+    data_train, data_test = load_encode_dataset()
+
+    train_config: dict = load_yaml("src/text_classification/config/classifier.yml")
+
+    update_params_composite_classifiers(train_config)
+
+    if DO_GRID_SEARCH:
+        clf = grid_search_best_params(data_train, train_config)
+    else:
+        clf = train_classifier(data_train, train_config)
+
+    evaluate_metrics(clf, data_test)
+
+    # neural_classifier(data_train, data_test)
 
 
 if __name__ == "__main__":
