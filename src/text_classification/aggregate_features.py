7996091a1349580fab3356cdbd658797b0fa30f0
Matteo Marcuzzo <matteo.marcuzzo.it@gmail.com>
2023-11-13 19:34:17 +0100

    Pyfuming

--- src/text_classification/aggregate_features.py
+++ src/text_classification/aggregate_features.py
@@ -6,6 +6,7 @@ import pandas as pd
 
 from src.deep_learning_strategy.classes.AMI2018Dataset import AMI2018Dataset
 from src.deep_learning_strategy.classes.CGReviewDataset import CGReviewDataset
+from src.deep_learning_strategy.classes.CallMeSexistDataset import CallMeSexistDataset
 from src.deep_learning_strategy.classes.Dataset import AbcDataset
 from src.text_classification.classes.features.ChatGPTDetector import ChatGPTDetector
 from src.text_classification.classes.features.EmotionLex import EmotionLex
@@ -23,7 +24,7 @@ from src.text_classification.classes.features.TopicLM import TopicLM
 from src.text_classification.classes.features.Wellformedness import Wellformedness
 
 
-def compute_features(dataset_: AbcDataset):
+def compute_features(dataset_: AbcDataset, do_label: bool = False):
     data = dataset_.get_train_val_test_split()
 
     train_texts: list[str] = data["train"]["x"]
@@ -32,7 +33,8 @@ def compute_features(dataset_: AbcDataset):
     len_train: int = len(train_texts)
     train_texts.extend(test_texts)
 
-    feature_transforms = [EvidenceType, TopicLM, Wellformedness, ChatGPTDetector, ParrotAdequacy, GibberishDetector, TextEmotion, TextPolarity, TextGrammarErrors]
+    feature_transforms = [EvidenceType, TopicLM, Wellformedness, ChatGPTDetector,
+                          ParrotAdequacy, GibberishDetector, TextEmotion, TextPolarity, TextGrammarErrors]
     feature_transforms += [TextStatistics, LIWCFeatures, EmpathFeatures, EmotionLex]
     feature_transforms = [f(use_gpu=True) for f in feature_transforms]
 
@@ -44,12 +46,21 @@ def compute_features(dataset_: AbcDataset):
     train_features = {k: f[:len_train] for k, f in all_features.items()}
     test_features = {k: f[len_train:] for k, f in all_features.items()}
 
-    pd.DataFrame.from_dict(train_features).to_csv(Path(dataset_.BASE_DATASET) / f"{dataset_.__class__.__name__}_train_features.csv", index=False)
-    pd.DataFrame.from_dict(test_features).to_csv(Path(dataset_.BASE_DATASET) / f"{dataset_.__class__.__name__}_test_features.csv", index=False)
+    if do_label:
+        df = pd.DataFrame.from_dict(train_features)
+        df["label"] = data["train"]["y"]
+        df.to_csv(Path(dataset_.BASE_DATASET) / f"{dataset_.__class__.__name__}_train_features.csv", index=False)
+
+        df = pd.DataFrame.from_dict(test_features)
+        df["label"] = data["test"]["y"]
+        df.to_csv(Path(dataset_.BASE_DATASET) / f"{dataset_.__class__.__name__}_test_features.csv", index=False)
+    else:
+        pd.DataFrame.from_dict(train_features).to_csv(Path(dataset_.BASE_DATASET) / f"{dataset_.__class__.__name__}_train_features.csv", index=False)
+        pd.DataFrame.from_dict(test_features).to_csv(Path(dataset_.BASE_DATASET) / f"{dataset_.__class__.__name__}_test_features.csv", index=False)
 
 
 if __name__ == "__main__":
     # data_path = Path("dataset") / "fake_reviews_dataset.csv"
-    dataset = AMI2018Dataset()
+    dataset = CallMeSexistDataset()
     # dataset = CGReviewDataset(test_size=0.25)
-    compute_features(dataset)
+    compute_features(dataset, do_label=True)
