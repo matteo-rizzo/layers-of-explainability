b4412e20c6434b4d29bc6b80c59609c83575ecd8
alek9z <alessandro.zangari.code@outlook.com>
2023-11-20 16:12:38 +0100

    Shap fix

--- src/text_classification/explain_classifier.py
+++ src/text_classification/explain_classifier.py
@@ -4,18 +4,21 @@ from pathlib import Path
 
 import joblib
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
 from skorch import NeuralNetBinaryClassifier
 
 from src.deep_learning_strategy.classes.CGReviewDataset import CGReviewDataset
+from src.deep_learning_strategy.classes.CallMeSexistDataset import CallMeSexistDataset
 from src.deep_learning_strategy.classes.CategoricalShapExplainer import CategoricalShapExplainer
 from src.deep_learning_strategy.classes.Dataset import AbcDataset
+from src.deep_learning_strategy.classes.IMDBDataset import IMDBDataset
 from src.text_classification.classes.training.TrainingModelUtility import TrainingModelUtility
 from src.text_classification.utils import load_encode_dataset
 from src.utils.yaml_manager import load_yaml
 
-DATASET: AbcDataset = CGReviewDataset()
-SK_CLASSIFIER_TYPE: type = RandomForestClassifier
-LOAD_MODEL_DUMP = Path("dumps") / "nlp_models" / "RandomForestClassifier" / "model_1698656887.6983652.pkl"
+DATASET: AbcDataset = IMDBDataset()
+SK_CLASSIFIER_TYPE: type = LogisticRegression
+LOAD_MODEL_DUMP = Path("dumps") / "nlp_models" / "LogisticRegression" / "model_1700476850.319165.pkl"
 
 
 # SK_CLASSIFIER_TYPE: type = NeuralNetBinaryClassifier
@@ -24,10 +27,9 @@ LOAD_MODEL_DUMP = Path("dumps") / "nlp_models" / "RandomForestClassifier" / "mod
 
 def main():
     # Load data
-    data_train, data_test = load_encode_dataset()
+    data_train, data_test = load_encode_dataset(dataset=DATASET, scale=True, features=None)
     train_config: dict = load_yaml("src/text_classification/config/classifier.yml")
-    y_train = data_train.pop("y")
-    # y_test = data_test.pop("y")
+    data_train.pop("y")
 
     # Load model
     clf = joblib.load(LOAD_MODEL_DUMP)
@@ -38,7 +40,7 @@ def main():
     tmu.evaluate(data_test, DATASET.compute_metrics)
 
     explainer = CategoricalShapExplainer(clf)
-    explainer.run(data_train, data_test.iloc[0:500, :], output_names=["real", "fake"], make_tensors=False)
+    explainer.run(data_train, data_test.iloc[0:500, :], output_names=["real", "fake"])
 
 
 if __name__ == "__main__":
