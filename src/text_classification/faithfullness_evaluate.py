433ad088b7909b22332c79850fcf45dee2d4fe6c
alek9z <alessandro.zangari.code@outlook.com>
2023-12-07 11:54:57 +0100

    Have faith

--- src/text_classification/faithfullness_evaluate.py
+++ src/text_classification/faithfullness_evaluate.py
@@ -15,35 +15,50 @@ from src.text_classification.easy_classifier import EXCLUDE_LIST
 from src.text_classification.utils import load_encode_dataset
 
 DATASET: AbcDataset = CallMeSexistDataset()
-LOAD_MODEL_DUMP = Path("dumps") / "nlp_models" / "XGBClassifier" / "model_CMS_FINAL.pkl"
+LOAD_MODEL_DUMP = Path("dumps") / "nlp_models" / "XGBClassifier" / "model_CMS_FINAL_RFE.pkl"
 
 
-def replace_with_column_average(arr, col_indices):
+def _replace_with_column_average(arr, col_indices, feature_neutral: np.ndarray):
     # Calculate the column averages
-    col_avgs = np.mean(arr[:, col_indices], axis=0)
+    # col_avgs = feature_neutral  # np.zeros(arr.shape[1])  # np.mean(arr, axis=0)
 
     # Replace the values in the specified columns with the column averages
-    arr[:, col_indices] = col_avgs
+    for i in range(arr.shape[0]):
+        arr[i, col_indices[i, :]] = feature_neutral[col_indices[i, :]]
 
     return arr
 
 
-def faith_metrics(test_data: pd.DataFrame, model, shap_values_abs, base_probs: np.ndarray, top_k: int):
-    shap_top_k = np.argpartition(shap_values_abs, -top_k, axis=1)[:, -top_k:]
-    shap_not_top_k = set(range(test_data.shape[1])) - set(shap_top_k.tolist())
+def _complementary_indices(arr, top_k_indices):
+    # Get all indices
+    all_indices = np.arange(arr.shape[1])
 
-    test_data_suff = replace_with_column_average(test_data.copy().to_numpy(), shap_top_k)
-    test_data_comp = replace_with_column_average(test_data.copy().to_numpy(), shap_top_k)
+    # Initialize an empty array to store the complementary indices
+    comp_indices = np.empty((arr.shape[0], arr.shape[1] - top_k_indices.shape[1]), dtype=int)
 
-    probs_suff = model.predict_proba(test_data_suff)
-    probs_comp = model.predict_proba(test_data_comp)
+    # For each row, find the complementary indices
+    for i in range(arr.shape[0]):
+        comp_indices[i] = np.setdiff1d(all_indices, top_k_indices[i])
+
+    return comp_indices
+
+
+def compute_faith_metrics(test_data: pd.DataFrame, model, shap_values_abs, base_probs: np.ndarray, feature_neutral: np.ndarray, top_k: int):
+    shap_top_k = np.argpartition(shap_values_abs, -top_k, axis=1)[:, -top_k:]  # (samples, top_k)
+    shap_not_top_k = _complementary_indices(shap_values_abs, shap_top_k)
+
+    test_data_suff = _replace_with_column_average(test_data.copy().to_numpy(), shap_top_k, feature_neutral)
+    test_data_comp = _replace_with_column_average(test_data.copy().to_numpy(), shap_not_top_k, feature_neutral)
+
+    probs_suff = model.predict_proba(test_data_suff)[:, 1]
+    probs_comp = model.predict_proba(test_data_comp)[:, 1]
 
     suff = base_probs - probs_suff
     comp = base_probs - probs_comp
     return comp, suff
 
 
-def evaluate_faith(test_data: pd.DataFrame, model) -> dict:
+def evaluation_faith(test_data: pd.DataFrame, model, feature_neutral: np.ndarray, q: list[int] = [1, 5, 10, 20]) -> dict:
     probs = model.predict_proba(test_data)[:, 1]
 
     explainer = shap.TreeExplainer(model)
@@ -56,21 +71,29 @@ def evaluate_faith(test_data: pd.DataFrame, model) -> dict:
     # shap_values_relative_change_df = pd.DataFrame(shap_values_relative_change, columns=test_data.columns, index=test_data.index)
 
     metrics = defaultdict(list)
-    for k in [1, 5, 10]:
-        comp, suff = faith_metrics(test_data, model, shap_values_abs, probs, k)
+    for k in q:
+        comp, suff = compute_faith_metrics(test_data, model, shap_values_abs, probs, feature_neutral, k)
         metrics["comp"].append(comp)
         metrics["suff"].append(suff)
 
     return {k: float(np.mean(v)) for k, v in metrics.items()}
 
 
-def faith_eval():
+def main():
     data_train, data_test = load_encode_dataset(dataset=DATASET, max_scale=True, exclude_features=EXCLUDE_LIST)
     data_train.pop("y")
     y_true_test = data_test.pop("y")
 
+    # Feature neutral value
+    # FIXME: consider using a sampling distribution
+    feature_neutral = np.ones((data_test.shape[1],), dtype=float) * -1  # np.mean(data_train.to_numpy(), axis=0)
+
     # Load model
     clf = joblib.load(LOAD_MODEL_DUMP)
 
-    metrics = evaluate_faith(data_test, clf)
+    metrics = evaluation_faith(data_test, clf, feature_neutral)
     pprint(metrics)
+
+
+if __name__ == "__main__":
+    main()
