47142c286f8cbf179b5be901f89599e36d622261
alek9z <alessandro.zangari.code@outlook.com>
2023-12-14 10:03:48 +0100

    Faith analysis things, tried wilcoxon

--- src/text_classification/faithfullness_analysis.py
+++ src/text_classification/faithfullness_analysis.py
@@ -6,7 +6,7 @@ from pprint import pprint
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from scipy.stats import ttest_ind, norm, mannwhitneyu
+from scipy.stats import ttest_ind, norm, mannwhitneyu, wilcoxon
 import seaborn as sns
 
 from src.text_classification.external.statutils import stats_main
@@ -52,13 +52,33 @@ def plotbox(data1, data2, x_values: list, data1_lab: str, data2_lab: str, title:
     plt.show()
 
 
+def rank_biserial_correlation(a: np.ndarray, b: np.ndarray, au: float) -> float:
+    """
+    Compute the rank biserial correlation between two set of measurements
+
+    @param a: measurements for population a
+    @param b: measurements for population b
+    @param au: u-test statistic for population
+    @return: the rank-biserial correlation
+
+
+    # TODO: fix for Wilcoxon SRT
+    """
+    # Calculate the rank-biserial correlation as the effect size
+    n1 = len(a)
+    n2 = len(b)
+    rbc = 1 - (2 * au) / (n1 * n2)
+
+    return rbc
+
+
 def main(precision: int = 4):
     ks = [1, 5, 10, 20, 50, 75]
     ks_to_use = [0, 1, 2, 3, 4, 5]
     print(f"Using k values={[ks[i] for i in ks_to_use]}")
 
-    comp_lm = np.load("dumps/faithfulness/comp_lm.npy").astype(np.float64)[:, ks_to_use]
-    suff_lm = np.load("dumps/faithfulness/suff_lm.npy").astype(np.float64)[:, ks_to_use]
+    comp_lm = np.load("experiments/faithfulness/comp.npy").astype(np.float64)[:, ks_to_use]
+    suff_lm = np.load("experiments/faithfulness/suff.npy").astype(np.float64)[:, ks_to_use]
 
     comp_xg = np.load("dumps/faithfulness/comp_xg.npy").astype(np.float64)[:, ks_to_use]
     suff_xg = np.load("dumps/faithfulness/suff_xg.npy").astype(np.float64)[:, ks_to_use]
@@ -86,15 +106,19 @@ def main(precision: int = 4):
             path=out_path / "suff_boxplot")
 
     result = dict()
+    stat_method = wilcoxon
     for i in range(n_columns):
         # plothist(suff_xg[:, i])
         # plothist(suff_lm[:, i])
-        comp_r = mannwhitneyu(comp_xg[:, i], comp_lm[:, i], alternative="greater", nan_policy="raise")
-        suff_r = mannwhitneyu(suff_xg[:, i], suff_lm[:, i], alternative="less", nan_policy="raise")
+        u_comp, p_comp, *_ = stat_method(comp_xg[:, i], comp_lm[:, i], alternative="greater", nan_policy="raise")
+        u_suff, p_suff, *_ = stat_method(suff_xg[:, i], suff_lm[:, i], alternative="less", nan_policy="raise")
         # comp_r = ttest_ind(comp_xg[:, i], comp_lm[:, i], equal_var=False)
         # suff_r = ttest_ind(suff_xg[:, i], suff_lm[:, i], equal_var=False)
-        result[f"comp_{ks[ks_to_use[i]]}"] = float(np.round(comp_r.statistic, decimals=precision)), float(np.round(comp_r.pvalue, decimals=precision))
-        result[f"suff_{ks[ks_to_use[i]]}"] = float(np.round(suff_r.statistic, decimals=precision)), float(np.round(suff_r.pvalue, decimals=precision))
+
+        result[f"comp_{ks[ks_to_use[i]]}"] = (float(np.round(u_comp, decimals=precision)), float(np.round(p_comp, decimals=precision)),
+                                              rank_biserial_correlation(comp_xg[:, i], comp_lm[:, i], u_comp))
+        result[f"suff_{ks[ks_to_use[i]]}"] = (float(np.round(u_suff, decimals=precision)), float(np.round(p_suff, decimals=precision)),
+                                              rank_biserial_correlation(suff_xg[:, i], suff_lm[:, i], u_suff))
 
     pprint(result)
 
